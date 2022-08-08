890a2a19bcd0e8675e079f517ebd66747835f0b6
Arjun Balasingam <arjunvb@mit.edu>
2022-08-08 16:48:47 +0000

    Changes for microtel

--- pips_utils/samp.py
+++ pips_utils/samp.py
@@ -1,6 +1,7 @@
 import torch
-import utils.basic
+import pips_utils.basic
 import torch.nn.functional as F
+
 
 def bilinear_sample2d(im, x, y, return_inbounds=False):
     # x and y are each B, N
@@ -12,7 +13,7 @@ def bilinear_sample2d(im, x, y, return_inbounds=False):
     y = y.float()
     H_f = torch.tensor(H, dtype=torch.float32)
     W_f = torch.tensor(W, dtype=torch.float32)
-    
+
     # inbound_mask = (x>-0.5).float()*(y>-0.5).float()*(x<W_f+0.5).float()*(y<H_f+0.5).float()
 
     max_y = (H_f - 1).int()
@@ -22,7 +23,7 @@ def bilinear_sample2d(im, x, y, return_inbounds=False):
     x1 = x0 + 1
     y0 = torch.floor(y).int()
     y1 = y0 + 1
-    
+
     x0_clip = torch.clamp(x0, 0, max_x)
     x1_clip = torch.clamp(x1, 0, max_x)
     y0_clip = torch.clamp(y0, 0, max_y)
@@ -30,7 +31,7 @@ def bilinear_sample2d(im, x, y, return_inbounds=False):
     dim2 = W
     dim1 = W * H
 
-    base = torch.arange(0, B, dtype=torch.int64).cuda()*dim1
+    base = torch.arange(0, B, dtype=torch.int64).cuda() * dim1
     base = torch.reshape(base, [B, 1]).repeat([1, N])
 
     base_y0 = base + y0_clip * dim2
@@ -44,7 +45,7 @@ def bilinear_sample2d(im, x, y, return_inbounds=False):
     # use the indices to lookup pixels in the flat image
     # im is B x C x H x W
     # move C out to last dim
-    im_flat = (im.permute(0, 2, 3, 1)).reshape(B*H*W, C)
+    im_flat = (im.permute(0, 2, 3, 1)).reshape(B * H * W, C)
     i_y0_x0 = im_flat[idx_y0_x0.long()]
     i_y0_x1 = im_flat[idx_y0_x1.long()]
     i_y1_x0 = im_flat[idx_y1_x0.long()]
@@ -61,8 +62,9 @@ def bilinear_sample2d(im, x, y, return_inbounds=False):
     w_y1_x0 = ((x1_f - x) * (y - y0_f)).unsqueeze(2)
     w_y1_x1 = ((x - x0_f) * (y - y0_f)).unsqueeze(2)
 
-    output = w_y0_x0 * i_y0_x0 + w_y0_x1 * i_y0_x1 + \
-             w_y1_x0 * i_y1_x0 + w_y1_x1 * i_y1_x1
+    output = (
+        w_y0_x0 * i_y0_x0 + w_y0_x1 * i_y0_x1 + w_y1_x0 * i_y1_x0 + w_y1_x1 * i_y1_x1
+    )
     # output is B*N x C
     output = output.view(B, -1, C)
     output = output.permute(0, 2, 1)
@@ -72,7 +74,9 @@ def bilinear_sample2d(im, x, y, return_inbounds=False):
         x_valid = (x > -0.5).byte() & (x < float(W_f - 0.5)).byte()
         y_valid = (y > -0.5).byte() & (y < float(H_f - 0.5)).byte()
         inbounds = (x_valid & y_valid).float()
-        inbounds = inbounds.reshape(B, N) # something seems wrong here for B>1; i'm getting an error here (or downstream if i put -1)
+        inbounds = inbounds.reshape(
+            B, N
+        )  # something seems wrong here for B>1; i'm getting an error here (or downstream if i put -1)
         return output, inbounds
 
-    return output # B, C, N
+    return output  # B, C, N
